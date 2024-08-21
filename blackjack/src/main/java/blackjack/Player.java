fda2718fff1514e6d1e909ca206b00457294772a
Jassel Doong <44394379+JasselD@users.noreply.github.com>
2024-08-21 13:23:13 +1200

    update

--- blackjack/src/main/java/blackjack/Player.java
+++ blackjack/src/main/java/blackjack/Player.java
@@ -9,5 +9,36 @@ package blackjack;
  * @author jasseldoong
  */
 public class Player {
+    private String name;
+    private Hand hand;
+    
+    public Player(String name) {
+        this.name = name;
+        hand = new Hand();
+    }
+    
+    public void addCard(Card card) {
+        hand.addCard(card);
+    }
+    
+    public int getSum() {
+        return hand.getSum();
+    }
+    
+    public boolean isBust() {
+        return hand.isBust();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Hand getHand() {
+        return hand;
+    }
+    
+    public String toString() {
+        return name + "'s hand: " + hand;
+    }
     
 }
