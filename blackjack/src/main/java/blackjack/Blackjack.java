ee0312bba19e706951eb27ad285dc4c954f537e3
sanderengel <saet@itu.dk>
2024-08-21 14:22:51 +1200

    wrote input handler

--- blackjack/src/main/java/blackjack/Blackjack.java
+++ blackjack/src/main/java/blackjack/Blackjack.java
@@ -1,3 +1,5 @@
+// NOT DONE AT ALLLLLL
+
 package blackjack;
 
 import java.io.IOException;
@@ -8,24 +10,50 @@ import java.io.InputStream;
  * @author sanderengelthilo
  */
 public class Blackjack {
-	public static void main(String[] args) {
+	private Deck deck;
+	private Dealer dealer;
+	private Player player;
+	private Gamestate gameState;
+	// private InputHandler inputHandler;
+	private OutputHandler outputHandler;
+	
+	public void BlackJackGame() {
+		// Load deck
 		try {
 			// Load resource file
 			InputStream inputStream = Blackjack.class.getResourceAsStream("/cards.txt");
-			
+
 			if (inputStream == null) {
 				throw new IOException("Resource file not found");
 			}
-			
+
 			// Initialize new deck with input stream
-			Deck deck = new Deck(inputStream);
-			
+			deck = new Deck(inputStream);
+
+			// Shuffle deck
+			deck.shuffle();
+
+			// Print cards in deck for testing
 			for (Card card : deck.getCards()) {
 				System.out.println(card);
 			}
-			
+
 		} catch (IOException e) {
 			System.out.println("Error loading deck: " + e.getMessage());
 		}
+	}
+	
+	
+
+
+
+//	// Initialize other classes
+//	public BlackJackGame() {
+//		dealer = new Dealer("Dealer");
+//		player = new Player("Player");
+//	}
+	
+	public static void main(String[] args) {
+		BlackJackGame();
 	}
 }
---
+++ blackjack/src/main/java/blackjack/InputHandler.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sanderengelthilo
+ */
+public class InputHandler {
+	private Scanner scanner;
+	
+	public InputHandler() {
+		scanner = new Scanner(System.in);
+	}
+	
+	public String getAction() {
+		System.out.println("Do you wish to HIT (H) or STAY (S)? ");
+		String action = scanner.nextLine();
+		return action.toUpperCase();
+	}
+}
