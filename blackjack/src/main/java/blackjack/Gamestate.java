c5194fbce017cd5f9a1ccf13e0cf0d8ebbaa1d4d
Jassel Doong <44394379+JasselD@users.noreply.github.com>
2024-08-21 14:31:20 +1200

    Update Gamestate.java

--- blackjack/src/main/java/blackjack/Gamestate.java
+++ blackjack/src/main/java/blackjack/Gamestate.java
@@ -10,4 +10,36 @@ package blackjack;
  */
 public class Gamestate {
     
+    // Instance variables
+    private boolean gameOver;
+    private String result;
+    
+    public Gamestate() {
+        gameOver = false;
+    }
+    
+    public void setGameState(String result) {
+        this.gameOver = true;
+        this.result = result;
+    }
+    
+    public boolean isGameOver() {
+        return gameOver;
+    }
+    
+    public void winner(Player player, Dealer dealer) {
+        if(dealer.isBust()) {
+            setGameState("Dealer busts!"+ player.getName());
+        } else if( player.getSum() > dealer.getSum()) {
+            setGameState(player.getName() +" win");
+        } else if(player.getSum() < dealer.getSum()) {
+            setGameState(player.getName() +" lose");
+        } else{
+            setGameState("tie");
+        }
+    }
+    
+    public String getResult() {
+        return result;
+    }
 }
